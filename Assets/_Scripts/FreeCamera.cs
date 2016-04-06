8143392000b76257eb667d8cac95f20be07469e8
allen <goodmorningcmdr@users.noreply.github.com>
2016-04-05 22:44:54 -0400

    Update FreeCamera.cs

--- Assets/_Scripts/FreeCamera.cs
+++ Assets/_Scripts/FreeCamera.cs
@@ -1,29 +1,66 @@
 using UnityEngine;
+using UnityEngine.Events;
 
+[RequireComponent(typeof(Camera))]
 public class FreeCamera : MonoBehaviour {
 	public bool enabledOnPause = true, editorOnly = true;
-	float rotationY, rotationX;
+
+	[Space(20)]
 	public float lookSensitivity = 5;
 	public float moveSpeed = 5;
 	public float speedModifier = 2;
-	float _moveSpeed;
-	public string lookHorizontal = "Mouse X", lookVertical = "Mouse Y";
-	public KeyCode moveForward = KeyCode.W, moveBack = KeyCode.S, moveLeft = KeyCode.A, moveRight = KeyCode.D, moveFaster = KeyCode.LeftShift;
+
+	[Space(20)]
+	public string lookHorizontal = "Mouse X";
+	public string lookVertical = "Mouse Y";
+	public KeyCode moveForward = KeyCode.W, moveBack = KeyCode.S, moveLeft = KeyCode.A, moveRight = KeyCode.D, moveFaster = KeyCode.LeftShift, flyToggle = KeyCode.F8;
 	
+	[Space(20)]
+	public UnityEvent FlyEnabled, FlyDisabled;
+
+	float rotationY, rotationX;
+	float _moveSpeed;
+	bool _on = false;
+
+	public bool on {
+		get
+		{
+			return _on;
+		}
+		set
+		{	
+			_on = value;
+		
+			if (_on)
+			{
+				FlyEnabled.Invoke();
+			}
+			else
+			{
+				FlyDisabled.Invoke();
+			}
+		}
+	}
+
 	void OnEnable() {
 		if (!Application.isEditor && editorOnly) Destroy(this);
 	}
+	
 	void Start() {
 		_moveSpeed = moveSpeed;
 	}
-	
+
 	void Update() {
-		if (!enabledOnPause && Time.timeScale == 0) return;
+		if (Input.GetKeyDown(flyToggle))
+			on = !on;
+
+		if (!enabledOnPause && Time.timeScale == 0 || !on) return;
+
 		rotationX = transform.localEulerAngles.y + Input.GetAxisRaw(lookHorizontal) * lookSensitivity;
 		rotationY += Input.GetAxisRaw(lookVertical) * lookSensitivity; rotationY = Mathf.Clamp(rotationY, -90, 90);
-		
+
 		moveSpeed = (Input.GetKey(moveFaster)) ? _moveSpeed * speedModifier : _moveSpeed;
-		
+
 		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
 		if (Input.GetKey(moveForward)) transform.position += transform.forward * moveSpeed * Time.unscaledDeltaTime;
 		if (Input.GetKey(moveBack)) transform.position -= transform.forward * moveSpeed * Time.unscaledDeltaTime;
