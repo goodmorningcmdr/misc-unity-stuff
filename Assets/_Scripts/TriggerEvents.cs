912649fd7bcf7efeb5d754addc886a0fc41d9e47
allen <goodmorningcmdr@users.noreply.github.com>
2016-04-05 22:03:35 -0400

    Update TriggerEvents.cs

--- Assets/_Scripts/TriggerEvents.cs
+++ Assets/_Scripts/TriggerEvents.cs
@@ -3,16 +3,28 @@ using UnityEngine.Events;
 
 public class TriggerEvents : MonoBehaviour {
 
-    public UnityEvent OnEnter;
-    public UnityEvent OnExit;
-
+	public UnityEvent OnEnter;
+	public UnityEvent OnExit;
+	
 	public string onlyWithTag = "";
-
-    void OnTriggerEnter(Collider c) {
+	public bool checkTag = false;
+	
+	void Awake() {
+		if (onlyWithTag.Length <= 0)
+			checkTag = false;
+	}
+	
+	void OnTriggerEnter(Collider c) {
+		if (c.transform.tag != onlyWithTag && checkTag)
+			return;
+			
 		OnEnter.Invoke();
 	}
-    
+	
 	void OnTriggerExit(Collider c) {
+		if (c.transform.tag != onlyWithTag && checkTag)
+			return;
+			
 		OnExit.Invoke();
 	}
 }
