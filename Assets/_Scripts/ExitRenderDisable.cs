3afcf7ca7f66554591c7d509ae858b0bd3967ef7
allen <goodmorningcmdr@users.noreply.github.com>
2016-04-05 21:51:48 -0400

    Update ExitRenderDisable.cs

--- Assets/_Scripts/ExitRenderDisable.cs
+++ Assets/_Scripts/ExitRenderDisable.cs
@@ -2,24 +2,32 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class ExitRenderDisable : MonoBehaviour {
-	
-	Transform player;
+
+	public Transform target;
 	public bool distanceCheck;
 	public float maxDistance = 50;
 	[Range(0.001f, 60)]
 	public float interval = 1f;
-	
+
 	public UnityEvent OnVisible;
 	public UnityEvent OnInvisible;
 
 	void Awake() {
-		player = Camera.main.transform;
-		if (distanceCheck) InvokeRepeating("DistanceCheck", 1, interval);
+		if (target == null && Camera.main) target = Camera.main.transform;
+		if (distanceCheck && target) InvokeRepeating("DistanceCheck", 1, interval);
 	}
-	
+
 	void DistanceCheck() {
-		float dist = (transform.position - player.position).sqrMagnitude;
-		
+		if (!target)
+		{
+			distanceCheck = false;
+			CancelInvoke();
+			OnVisible.Invoke();
+			return;
+		}
+
+		float dist = (transform.position - target.position).sqrMagnitude;
+
 		if (dist > maxDistance * maxDistance)
 		{
 			OnInvisible.Invoke();
